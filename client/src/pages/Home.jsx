2efaf56c49f312e92c5365a7c279dbca973bd184
Omoyajowo Temidayo <temideewan@gmail.com>
2023-08-03 21:35:14 +0100

    feat: set up for landing page with framer motion

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  fadeAnimation,
+  slideAnimation,
+} from "../config/motion";
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div
+              className="home-content"
+              {...headTextAnimation}
+            >
+              <h1 className="head-text">
+                LET&apos;S <br className="xl:block hidden"/> DO IT
+              </h1>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
